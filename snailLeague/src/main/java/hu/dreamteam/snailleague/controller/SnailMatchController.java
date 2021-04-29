d4d9cfbfe246ad1539159341eeef8a4ee961d575
Zoltan Reviczky <reviczkyzoli@gmail.com>
2021-04-29 10:04:45 +0200

    Created SnailMatchcontroller and Usermatchcontroller

---
+++ snailLeague/src/main/java/hu/dreamteam/snailleague/controller/SnailMatchController.java
@@ -0,0 +1,80 @@
+package hu.dreamteam.snailleague.controller;
+
+import hu.dreamteam.snailleague.exception.ResourceNotFoundException;
+import hu.dreamteam.snailleague.model.Match;
+import hu.dreamteam.snailleague.model.Snail;
+import hu.dreamteam.snailleague.model.SnailMatch;
+import hu.dreamteam.snailleague.model.UserMatch;
+import hu.dreamteam.snailleague.repository.SnailMatchRepository;
+import hu.dreamteam.snailleague.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class SnailMatchController {
+
+    @Autowired
+    private SnailMatchRepository snailMatchRepository;
+    private UserRepository userRepository;
+
+    // create snailmatch
+    @PostMapping("/snailmatches")
+    public SnailMatch createSnailMatch(@RequestBody SnailMatch snailMatch) {
+        return this.snailMatchRepository.save(snailMatch);
+    }
+
+    // get snailmatches
+    @GetMapping("/snailmatches")
+    public List<SnailMatch> getAllSnailMatches() {
+        return this.snailMatchRepository.findAll();
+    }
+
+    // get snailmatch by id
+    @GetMapping("/snailmatches/{id}")
+    public ResponseEntity<SnailMatch> getSnailMatchById(@PathVariable(value = "id") Long snailMatchId) throws ResourceNotFoundException {
+        SnailMatch snailMatch = snailMatchRepository.findById(snailMatchId).orElseThrow(() -> new ResourceNotFoundException("SnailMatch not found by this id: " + snailMatchId));
+
+        return ResponseEntity.ok().body(snailMatch);
+    }
+
+    // update snailmatch assign match/user
+    @PutMapping("/snailmatches/{id}")
+    public ResponseEntity<SnailMatch> updateSnailMatchMatchUser(@PathVariable(value = "id") Long snailMatchId, @Validated @RequestBody Snail snail, @Validated @RequestBody Match match) throws ResourceNotFoundException {
+        SnailMatch snailMatch = snailMatchRepository.findById(snailMatchId).orElseThrow(() -> new ResourceNotFoundException("SnailMatch not found by the id : " + snailMatchId));
+
+        snailMatch.setMatch(match);
+        snailMatch.setSnail(snail);
+
+        return ResponseEntity.ok(this.snailMatchRepository.save(snailMatch));
+    }
+
+    // update snailmatch assign place
+    @PutMapping("/snailmatches/{id}")
+    public ResponseEntity<SnailMatch> updateSnailMatchPlace(@PathVariable(value = "id") Long snailMatchId, Integer place) throws ResourceNotFoundException {
+        SnailMatch snailMatch = snailMatchRepository.findById(snailMatchId).orElseThrow(() -> new ResourceNotFoundException("SnailMatch not found by the id : " + snailMatchId));
+
+        snailMatch.setPlace(place);
+
+        return ResponseEntity.ok(this.snailMatchRepository.save(snailMatch));
+    }
+
+    // delete snailmatch
+    @DeleteMapping("/snailmatches/{id}")
+    public Map<String, Boolean> deleteSnailMatch(@PathVariable(value = "id") Long snailMatchId) throws ResourceNotFoundException {
+        SnailMatch snailMatch = snailMatchRepository.findById(snailMatchId).orElseThrow(() -> new ResourceNotFoundException("SnailMatch not found by the id: " + snailMatchId));
+
+        this.snailMatchRepository.delete(snailMatch);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+
+        return response;
+    }
+}
---
+++ snailLeague/src/main/java/hu/dreamteam/snailleague/controller/UserMatchController.java
@@ -0,0 +1,83 @@
+package hu.dreamteam.snailleague.controller;
+
+import hu.dreamteam.snailleague.exception.ResourceNotFoundException;
+import hu.dreamteam.snailleague.model.Match;
+import hu.dreamteam.snailleague.model.Snail;
+import hu.dreamteam.snailleague.model.User;
+import hu.dreamteam.snailleague.model.UserMatch;
+import hu.dreamteam.snailleague.repository.UserMatchRepository;
+import hu.dreamteam.snailleague.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class UserMatchController {
+
+    @Autowired
+    private UserMatchRepository userMatchRepository;
+    private UserRepository userRepository;
+
+    // create usermatch
+    @PostMapping("/usermatches")
+    public UserMatch createUserMatch(@RequestBody UserMatch userMatch) {
+        return this.userMatchRepository.save(userMatch);
+    }
+
+    // get usermatches
+    @GetMapping("/usermatches")
+    public List<UserMatch> getAllUserMatches() {
+        return this.userMatchRepository.findAll();
+    }
+
+    // get usermatch by id
+    @GetMapping("/usermatches/{id}")
+    public ResponseEntity<UserMatch> getUserMatchById(@PathVariable(value = "id") Long userMatchId) throws ResourceNotFoundException {
+        UserMatch userMatch = userMatchRepository.findById(userMatchId).orElseThrow(() -> new ResourceNotFoundException("UserMatch not found by this id: " + userMatchId));
+
+        return ResponseEntity.ok().body(userMatch);
+    }
+
+    // update usermatch set match,user,snail
+    @PutMapping("/usermatches/{id}")
+    public ResponseEntity<UserMatch> updateuserMatchMatchUser(@PathVariable(value = "id") Long userMatchId, @Validated @RequestBody User user, @Validated @RequestBody Match match, @Validated @RequestBody Snail snail) throws ResourceNotFoundException {
+        UserMatch userMatch = userMatchRepository.findById(userMatchId).orElseThrow(() -> new ResourceNotFoundException("UserMatch not found by the id : " + userMatchId));
+
+        userMatch.setMatch(match);
+        userMatch.setSnailId(snail);
+        userMatch.setUser(user);
+
+        return ResponseEntity.ok(this.userMatchRepository.save(userMatch));
+    }
+
+    // update usermatch assign win,money
+    @PutMapping("/usermatches/{id}")
+    public ResponseEntity<UserMatch> updateUserMatchPlace(@PathVariable(value = "id") Long userMatchId, Integer money, Boolean win) throws ResourceNotFoundException {
+        UserMatch userMatch = userMatchRepository.findById(userMatchId).orElseThrow(() -> new ResourceNotFoundException("UserMatch not found by the id : " + userMatchId));
+
+        userMatch.setWon(win);
+        userMatch.setMoneySet(money);
+
+        return ResponseEntity.ok(this.userMatchRepository.save(userMatch));
+    }
+
+    // delete usermatch
+    @DeleteMapping("/usermatches/{id}")
+    public Map<String, Boolean> deleteUserMatch(@PathVariable(value = "id") Long userMatchId) throws ResourceNotFoundException {
+        UserMatch userMatch = userMatchRepository.findById(userMatchId).orElseThrow(() -> new ResourceNotFoundException("UserMatch not found by the id: " + userMatchId));
+
+        this.userMatchRepository.delete(userMatch);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+
+        return response;
+    }
+}
+
